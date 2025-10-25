ce03aab08d4580a26ff4baf7c3c53ba75047256e
ombhojwani178 <bhojwaniom068@gmail.com>
2025-10-25 22:21:34 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import "leaflet/dist/leaflet.css";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Home from "./Home";
@@ -13,31 +12,36 @@ import Userchoice from "./Userchoice";
 import Getchoice from "./Getchoice";
 import Donate from "./Donate";
 import Receive from "./Receive";
-import Receiveresults from "./Receiveresults";
+import ReceiveResults from "./ReceiveResults";
+import Cart from "./Cart";
+import { CartProvider } from "./Cartcontext"; // ✅ Correct import
 
 function App() {
   return (
     <Router>
-      <div className="flex flex-col min-h-screen">
-        <Navbar />
-        <main className="flex-grow">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<Register />} />
-            <Route path="/dashboard" element={<Dashboard />} />
-             <Route path="/button" element={<Button />} />
+      <CartProvider>
+        <div className="flex flex-col min-h-screen">
+          <Navbar />
+          <main className="flex-grow">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/about" element={<About />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/register" element={<Register />} />
+              <Route path="/dashboard" element={<Dashboard />} />
+              <Route path="/button" element={<Button />} />
               <Route path="/card" element={<Card />} />
               <Route path="/userchoice" element={<Userchoice />} />
-              <Route path= "/getchoice" element={<Getchoice />} />
+              <Route path="/getchoice" element={<Getchoice />} />
               <Route path="/donate" element={<Donate />} />
               <Route path="/receive" element={<Receive />} />
-              <Route path="/receiveresults" element={<Receiveresults />} />
-          </Routes>
-        </main>
-        <Footer />
-      </div>
+              <Route path="/receiveresults" element={<ReceiveResults />} />
+              <Route path="/cart" element={<Cart />} />
+            </Routes>
+          </main>
+          <Footer />
+        </div>
+      </CartProvider>
     </Router>
   );
 }
